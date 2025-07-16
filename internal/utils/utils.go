1191e1f0312a1c065193b1da439258f9c5a0904a
Kostiantyn Kulbachnyi <kostiantyn@kulbachnyi.com>
2025-07-16 17:42:41 +0300

    added logging from #132 by @anasinnyk

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -3,12 +3,27 @@ package utils
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 )
+
+var verbose bool
+
+func init() {
+	verboseEnv := GetOSVar("VERBOSE")
+	verbose = strings.ToLower(verboseEnv) == "true" || verboseEnv == "1"
+}
 
 func RespError(err error) error {
 	if err != nil {
-		errMsg := fmt.Sprintf("there was an error during the call execution: %s\n", err)
+		errMsg := fmt.Sprintf("there was an error during the call execution: %s", err)
+
+		if verbose {
+			log.SetOutput(os.Stderr)
+			log.Printf("ERROR: %s", errMsg)
+		}
+
 		return errors.New(errMsg)
 	}
 	return nil
